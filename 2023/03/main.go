ddd9ee643685d37c33079d882e9ef1dd98b661f8
teleivo <teleivo@users.noreply.github.com>
2023-12-03 10:44:10 +0100

    wip day 3
    parse line numbers

---
+++ 2023/03/main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"unicode"
+)
+
+func main() {
+	if err := run(os.Stdout, os.Args); err != nil {
+		fmt.Printf("failed to solve puzzle due to: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func run(w io.Writer, args []string) error {
+	if len(args) != 2 {
+		return fmt.Errorf("expected one argument pointing to puzzle input, instead got %d args", len(args)-1)
+	}
+
+	file := args[1]
+
+	f, err := os.Open(file)
+	if err != nil {
+		return fmt.Errorf("failed to open file %q: %v", file, err)
+	}
+	defer f.Close()
+	cal, err := solvePartOne(f)
+	if err != nil {
+		return err
+	}
+	fmt.Fprintf(w, "The solution to puzzle one is: %d\n", cal)
+
+	return nil
+}
+
+// solvePartOne solves part one of the puzzle.
+func solvePartOne(r io.Reader) (int, error) {
+	// var sum int
+	// sc := bufio.NewScanner(r)
+	// for sc.Scan() {
+	// 	vals, err := parseLine(sc.Text())
+	// 	if err != nil {
+	// 		return 0, err
+	// 	}
+	// 	for _, v := range vals {
+	// 		sum += v
+	// 	}
+	// }
+	// return sum, nil
+	return 0, nil
+}
+
+type line struct {
+	Numbers []number
+	Symbols []number
+}
+
+type number struct {
+	Value int
+	Start int
+	End   int
+}
+
+func parseLine(in string) (line, error) {
+
+	var res line
+	var num *number
+
+	for i, c := range in {
+		if unicode.IsDigit(c) {
+			if num == nil {
+				num = &number{Start: i, End: i}
+			}
+		} else {
+			if num != nil {
+				num.End = i - 1
+				v, err := strconv.Atoi(in[num.Start : num.End+1])
+				if err != nil {
+					return res, fmt.Errorf("failed to parse number: %v", err)
+				}
+				num.Value = v
+				res.Numbers = append(res.Numbers, *num)
+				num = nil
+			}
+		}
+	}
+
+	return res, nil
+}
---
+++ 2023/03/main_test.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+// func TestSolve(t *testing.T) {
+// 	tests := []struct {
+// 		in   string
+// 		want int
+// 	}{
+// 		{
+// 			in: `467..114..
+// ...*......
+// ..35..633.
+// ......#...
+// 617*......
+// .....+.58.
+// ..592.....
+// ......755.
+// ...$.*....
+// .664.598..`,
+// 			want: 4361,
+// 		},
+// 	}
+//
+// 	for _, tc := range tests {
+// 		got, err := solvePartOne(strings.NewReader(tc.in))
+// 		if err != nil {
+// 			t.Fatalf("expected no error instead got %v", err)
+// 		}
+//
+// 		if got != tc.want {
+// 			t.Errorf("solvePartOne(%q) = %d; want %d", tc.in, got, tc.want)
+// 		}
+// 	}
+// }
+
+func TestParseLine(t *testing.T) {
+	tests := []struct {
+		in   string
+		want line
+	}{
+		{
+			in: "467..114..",
+			want: line{
+				Numbers: []number{
+					{Value: 467, Start: 0, End: 2},
+					{Value: 114, Start: 5, End: 7},
+				},
+				Symbols: nil,
+			},
+		},
+		{
+			in: "617*......",
+			want: line{
+				Numbers: []number{
+					{Value: 617, Start: 0, End: 2},
+				},
+				Symbols: nil,
+			},
+		},
+	}
+
+	for _, tc := range tests {
+		got, err := parseLine(tc.in)
+		if err != nil {
+			t.Fatalf("expected no error instead got %v", err)
+		}
+
+		if diff := cmp.Diff(tc.want, got); diff != "" {
+			t.Errorf("parseLine() mismatch (-want +got):\n%s", diff)
+		}
+	}
+
+}
