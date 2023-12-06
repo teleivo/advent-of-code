bf3fc6edcbe6f85f6215e07fb599737669c9b203
teleivo <teleivo@users.noreply.github.com>
2023-12-06 11:40:59 +0100

    07/part 1: init

---
+++ 2023/07/main_test.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestSolvePartOne(t *testing.T) {
+	tests := []struct {
+		in   string
+		want int
+	}{
+		{
+			in:   ``,
+			want: 30,
+		},
+	}
+
+	for _, tc := range tests {
+		got, err := solvePartTwo(strings.NewReader(tc.in))
+		assertNoError(t, err)
+		assertEquals(t, "solvePartOne", tc.in, tc.want, got)
+	}
+}
+
+func TestSolvePartTwo(t *testing.T) {
+	tests := []struct {
+		in   string
+		want int
+	}{
+		{
+			in:   ``,
+			want: 30,
+		},
+	}
+
+	for _, tc := range tests {
+		got, err := solvePartTwo(strings.NewReader(tc.in))
+		assertNoError(t, err)
+		assertEquals(t, "solvePartTwo", tc.in, tc.want, got)
+	}
+}
+
+func assertError(t *testing.T, err error) {
+	if err == nil {
+		t.Fatal("expected error instead got nil instead", err)
+	}
+}
+
+func assertNoError(t *testing.T, err error) {
+	if err != nil {
+		t.Fatalf("expected no error instead got: %q", err)
+	}
+}
+
+func assertEquals(t *testing.T, method string, in, want, got any) {
+	if got != want {
+		t.Errorf("%s(%q) = %d; want %d", method, in, got, want)
+	}
+}
+
+func assertDeepEquals(t *testing.T, method string, in, want, got any) {
+	if diff := cmp.Diff(want, got); diff != "" {
+		t.Errorf("%s(%q) mismatch (-want +got):\n%s", method, in, diff)
+	}
+}
