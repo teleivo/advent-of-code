95e84eb4a105c65d1d9fb99c22eaa54cdfbe4672
teleivo <teleivo@users.noreply.github.com>
2023-12-06 05:38:56 +0100

    06/part 1: solves example

---
+++ 2023/06/go.mod
@@ -0,0 +1,5 @@
+module teleivo/advent-of-code/2023/06
+
+go 1.21.1
+
+require github.com/google/go-cmp v0.6.0
---
+++ 2023/06/main.go
@@ -0,0 +1,130 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	if err := run(os.Stdout, os.Args); err != nil {
+		fmt.Printf("failed to solve puzzle due to: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func run(w io.Writer, args []string) error {
+	if len(args) != 2 {
+		return fmt.Errorf("expected one argument pointing to puzzle input, instead got %d args", len(args)-1)
+	}
+
+	file := args[1]
+
+	f1, err := os.Open(file)
+	if err != nil {
+		return fmt.Errorf("failed to open file %q: %v", file, err)
+	}
+	defer f1.Close()
+	cal, err := solvePartOne(f1)
+	if err != nil {
+		return err
+	}
+	fmt.Fprintf(w, "The solution to puzzle one is: %d\n", cal)
+
+	f2, err := os.Open(file)
+	if err != nil {
+		return fmt.Errorf("failed to open file %q: %v", file, err)
+	}
+	defer f2.Close()
+	cal, err = solvePartTwo(f2)
+	if err != nil {
+		return err
+	}
+	fmt.Fprintf(w, "The solution to puzzle two is: %d\n", cal)
+
+	return nil
+}
+
+// solvePartOne solves part one of the puzzle.
+func solvePartOne(r io.Reader) (int, error) {
+	br := bufio.NewReader(r)
+	durations, err := parseLine(br)
+	if err != nil {
+		return 0, err
+	}
+	fmt.Println("Time:", durations)
+
+	distances, err := parseLine(br)
+	if err != nil {
+		return 0, err
+	}
+	fmt.Println("Distance:", distances)
+
+	var result int
+	for i, duration := range durations {
+		// TODO simplify with handling the odd case
+		var sum int
+		for j := 0; j < duration+1; j++ {
+			if isBreakingRecord(j, duration, distances[i]) {
+				sum++
+			}
+		}
+
+		if result == 0 {
+			result = sum
+		} else {
+			result *= sum
+		}
+	}
+
+	return result, nil
+}
+
+func isBreakingRecord(timeHeld, raceDuration, recordDistance int) bool {
+	if timeHeld*(raceDuration-timeHeld) > recordDistance {
+		return true
+	}
+	return false
+
+}
+
+// solvePartOne solves part two of the puzzle.
+func solvePartTwo(r io.Reader) (int, error) {
+	return 0, nil
+}
+
+func parseLine(r *bufio.Reader) ([]int, error) {
+	in, err := r.ReadString('\n')
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse seeds in: %q", in)
+	}
+
+	_, seeds, found := strings.Cut(in, ": ")
+	if !found {
+		return nil, fmt.Errorf("failed to parse seeds in: %q", in)
+	}
+
+	// TODO I wanted to make parseNumbers reusable but sharing the reader is not smooth so far
+	return parseNumbers(strings.NewReader(seeds))
+}
+
+func parseNumbers(r io.Reader) ([]int, error) {
+	scanner := bufio.NewScanner(r)
+	scanner.Split(bufio.ScanWords)
+	var nums []int
+	for scanner.Scan() {
+		num, err := strconv.Atoi(scanner.Text())
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse number: %v", err)
+		}
+		nums = append(nums, num)
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, fmt.Errorf("failed to parse numbers: %v", err)
+	}
+	return nums, nil
+}
