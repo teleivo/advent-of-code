78de9e8b9a025fddcd3b29be80cf6c145b501f31
teleivo <teleivo@users.noreply.github.com>
2023-12-08 05:40:16 +0100

    08/part 1: init

---
+++ 2023/08/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+func main() {
+	if err := run(os.Stdout, os.Args); err != nil {
+		fmt.Printf("failed to solve puzzle due to: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func run(w io.Writer, args []string) error {
+	if len(args) != 2 {
+		return fmt.Errorf("expected one argument pointing to puzzle input, instead got %d args", len(args)-1)
+	}
+
+	file := args[1]
+
+	f1, err := os.Open(file)
+	if err != nil {
+		return fmt.Errorf("failed to open file %q: %v", file, err)
+	}
+	defer f1.Close()
+	cal, err := solvePartOne(f1)
+	if err != nil {
+		return err
+	}
+	fmt.Fprintf(w, "The solution to puzzle one is: %d\n", cal)
+
+	f2, err := os.Open(file)
+	if err != nil {
+		return fmt.Errorf("failed to open file %q: %v", file, err)
+	}
+	defer f2.Close()
+	cal, err = solvePartTwo(f2)
+	if err != nil {
+		return err
+	}
+	fmt.Fprintf(w, "The solution to puzzle two is: %d\n", cal)
+
+	return nil
+}
+
+// solvePartOne solves part one of the puzzle.
+func solvePartOne(r io.Reader) (int, error) {
+	return 0, nil
+}
+
+// solvePartOne solves part two of the puzzle.
+func solvePartTwo(r io.Reader) (int, error) {
+	return 0, nil
+}
