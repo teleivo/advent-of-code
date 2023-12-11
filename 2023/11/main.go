f23008427da6660c1d161a1536aca74bb00e3e02
teleivo <teleivo@users.noreply.github.com>
2023-12-11 11:40:31 +0100

    11/part 2: init

--- 2023/11/main.go
+++ 2023/11/main.go
@@ -231,12 +231,143 @@ func sumDistances(galaxies map[int][2]int, distances map[int][][]int) int {
 	return sum
 }
 
-// solvePartOne solves part two of the puzzle.
+// solvePartTwo solves part two of the puzzle.
 func solvePartTwo(r io.Reader) (int, error) {
+	grid, galaxies := parseAndExpandGridPartTwo(r)
+	// grid := parseAndExpandGrid(r)
+	// galaxies := enumerateGalaxies(grid)
+	distances := bfsPartTwo(grid, galaxies)
+	sum := sumDistances(galaxies, distances)
+	return sum, nil
+}
+
+func parseAndExpandGridPartTwo(r io.Reader) ([][]rune, map[int][2]int) {
 	s := bufio.NewScanner(r)
+	var grid [][]rune
+	var galaxyCount int
+	var rows, cols int
+	rowsWithoutGalaxy := make(map[int]struct{})
+	colsWithGalaxy := make(map[int]struct{})
 	for s.Scan() {
 		line := s.Text()
-		_ = line
+		cols = len(line)
+		var rowHasGalaxy bool
+		var row []rune
+		for col, r := range line {
+			if r == '#' {
+				galaxyCount++
+				rowHasGalaxy = true
+				colsWithGalaxy[col] = struct{}{}
+			}
+			row = append(row, r)
+		}
+		if !rowHasGalaxy {
+			rowsWithoutGalaxy[rows] = struct{}{}
+		}
+
+		grid = append(grid, row)
+		rows++
 	}
-	return 0, nil
+
+	// fmt.Println("unexpanded rows", rows, "cols", cols)
+	colsWithoutGalaxy := make(map[int]struct{})
+	for i := 0; i < cols; i++ {
+		if _, ok := colsWithGalaxy[i]; !ok {
+			colsWithoutGalaxy[i] = struct{}{}
+		}
+	}
+
+	var expandedRows [][]rune
+	for i := 0; i < rows; i++ {
+		expandedRows = append(expandedRows, grid[i])
+		if _, ok := rowsWithoutGalaxy[i]; ok {
+			expandedRows = append(expandedRows, grid[i])
+		}
+	}
+
+	var expandedGrid [][]rune
+	for i := 0; i < len(expandedRows); i++ {
+		expandedRow := expandedRows[i]
+		var row []rune
+		for j := 0; j < len(expandedRow); j++ {
+			row = append(row, expandedRow[j])
+			if _, ok := colsWithGalaxy[j]; !ok {
+				row = append(row, expandedRow[j])
+			}
+		}
+		expandedGrid = append(expandedGrid, row)
+	}
+
+	// map of x,y coordinates
+	galaxyCount = 0
+	galaxies := make(map[int][2]int)
+	for i, row := range expandedGrid {
+		for j, r := range row {
+			if r == '#' {
+				galaxies[galaxyCount] = [2]int{i, j}
+				galaxyCount++
+			}
+		}
+	}
+
+	return expandedGrid, galaxies
+}
+
+func bfsPartTwo(grid [][]rune, galaxies map[int][2]int) map[int][][]int {
+	rows := len(grid)
+	cols := len(grid[0])
+	distances := make(map[int][][]int)
+
+	for galaxy, source := range galaxies {
+		distance := make([][]int, rows)
+		visited := make([][]bool, rows)
+
+		sRow, sCol := source[0], source[1]
+		if distance[sRow] == nil {
+			distance[sRow] = make([]int, cols)
+		}
+		distance[sRow][sCol] = 0
+
+		if visited[sRow] == nil {
+			visited[sRow] = make([]bool, cols)
+		}
+		visited[sRow][sCol] = true
+		queue := [][2]int{source}
+
+		for len(queue) != 0 {
+			// dequeue
+			// fmt.Println("queue before dequeu", queue)
+			v := queue[0]
+			vRow, vCol := v[0], v[1]
+			if len(queue) > 1 {
+				queue = queue[1:]
+			} else {
+				queue = nil
+			}
+			// fmt.Println("queue after dequeu", queue)
+
+			for _, w := range neighbors(rows, cols, v) {
+				wRow, wCol := w[0], w[1]
+				// fmt.Println("rows", rows, "cols", cols, "v", v, "neighbor", w)
+
+				if visited[wRow] == nil {
+					visited[wRow] = make([]bool, cols)
+				}
+				if visited[wRow][wCol] {
+					continue
+				}
+				visited[wRow][wCol] = true
+
+				if distance[wRow] == nil {
+					distance[wRow] = make([]int, cols)
+				}
+				distance[wRow][wCol] = distance[vRow][vCol] + 1
+				queue = append(queue, w)
+				// fmt.Println("queue", queue, "distanceTo", distanceTo)
+			}
+		}
+		distances[galaxy] = distance
+	}
+
+	return distances
 }
