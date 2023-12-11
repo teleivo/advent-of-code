3d5dfd4360c206f572053617e0bcead44b4cb77d
teleivo <teleivo@users.noreply.github.com>
2023-12-11 11:11:41 +0100

    11/part 1: solve

--- 2023/11/main.go
+++ 2023/11/main.go
@@ -50,9 +50,10 @@ func run(w io.Writer, args []string) error {
 func solvePartOne(r io.Reader) (int, error) {
 	grid := parseAndExpandGrid(r)
 	galaxies := enumerateGalaxies(grid)
-	fmt.Println("galaxies", galaxies)
-
-	return 0, nil
+	// fmt.Println("galaxies", galaxies)
+	distances := bfs(grid, galaxies)
+	sum := sumDistances(galaxies, distances)
+	return sum, nil
 }
 
 func parseAndExpandGrid(r io.Reader) [][]rune {
@@ -84,7 +85,7 @@ func parseAndExpandGrid(r io.Reader) [][]rune {
 		rows++
 	}
 
-	fmt.Println("unexpanded rows", rows, "cols", cols)
+	// fmt.Println("unexpanded rows", rows, "cols", cols)
 	colsWithoutGalaxy := make(map[int]struct{})
 	for i := 0; i < cols; i++ {
 		if _, ok := colsWithGalaxy[i]; !ok {
@@ -132,41 +133,103 @@ func enumerateGalaxies(grid [][]rune) map[int][2]int {
 	return galaxies
 }
 
-// func bfs(grid [][]rune, galaxies map[int][2]int) {
-// 	marked := make([]bool, len(graph))
-// 	distanceTo := make([]int, len(graph))
-// 	for i := 0; i < len(distanceTo); i++ {
-// 		distanceTo[i] = -1
-// 	}
-// 	distanceTo[source] = 0
-// 	marked[source] = true
-// 	queue := []int{source}
-//
-// 	for len(queue) != 0 {
-// 		// dequeue
-// 		// fmt.Println("queue before dequeu", queue)
-// 		v := queue[0]
-// 		if len(queue) > 1 {
-// 			queue = queue[1:]
-// 		} else {
-// 			queue = nil
-// 		}
-// 		// fmt.Println("queue after dequeu", queue)
-//
-// 		for w := range graph[v].neighbors {
-// 			if marked[w] {
-// 				continue
-// 			}
-//
-// 			distanceTo[w] = distanceTo[v] + 1
-// 			marked[w] = true
-// 			queue = append(queue, w)
-// 			// fmt.Println("queue", queue, "distanceTo", distanceTo)
-// 		}
-// 	}
-//
-// 	fmt.Println("distanceTo", distanceTo)
-// }
+func bfs(grid [][]rune, galaxies map[int][2]int) map[int][][]int {
+	rows := len(grid)
+	cols := len(grid[0])
+	distances := make(map[int][][]int)
+
+	for galaxy, source := range galaxies {
+		distance := make([][]int, rows)
+		visited := make([][]bool, rows)
+
+		sRow, sCol := source[0], source[1]
+		if distance[sRow] == nil {
+			distance[sRow] = make([]int, cols)
+		}
+		distance[sRow][sCol] = 0
+
+		if visited[sRow] == nil {
+			visited[sRow] = make([]bool, cols)
+		}
+		visited[sRow][sCol] = true
+		queue := [][2]int{source}
+
+		for len(queue) != 0 {
+			// dequeue
+			// fmt.Println("queue before dequeu", queue)
+			v := queue[0]
+			vRow, vCol := v[0], v[1]
+			if len(queue) > 1 {
+				queue = queue[1:]
+			} else {
+				queue = nil
+			}
+			// fmt.Println("queue after dequeu", queue)
+
+			for _, w := range neighbors(rows, cols, v) {
+				wRow, wCol := w[0], w[1]
+				// fmt.Println("rows", rows, "cols", cols, "v", v, "neighbor", w)
+
+				if visited[wRow] == nil {
+					visited[wRow] = make([]bool, cols)
+				}
+				if visited[wRow][wCol] {
+					continue
+				}
+				visited[wRow][wCol] = true
+
+				if distance[wRow] == nil {
+					distance[wRow] = make([]int, cols)
+				}
+				distance[wRow][wCol] = distance[vRow][vCol] + 1
+				queue = append(queue, w)
+				// fmt.Println("queue", queue, "distanceTo", distanceTo)
+			}
+		}
+		distances[galaxy] = distance
+	}
+
+	return distances
+}
+
+func neighbors(rows, cols int, cell [2]int) [][2]int {
+	moves := [4][2]int{
+		{0, 1},
+		{1, 0},
+		{0, -1},
+		{-1, 0},
+	}
+
+	var legalMoves [][2]int
+	for _, move := range moves {
+		nextRow := cell[0] + move[0]
+		nextCol := cell[1] + move[1]
+
+		if nextCol >= 0 && nextCol < cols && nextRow >= 0 && nextRow < rows {
+			legalMoves = append(legalMoves, [2]int{nextRow, nextCol})
+		}
+	}
+	return legalMoves
+}
+
+func sumDistances(galaxies map[int][2]int, distances map[int][][]int) int {
+	var sum int
+
+	for i := range galaxies {
+		for j := range galaxies {
+			if j < i+1 {
+				continue
+			}
+			// fmt.Printf("galaxy %d to %d\n", i, j)
+			distance := distances[i]
+			destination := galaxies[j]
+			dRow, dCol := destination[0], destination[1]
+			sum += distance[dRow][dCol]
+		}
+	}
+
+	return sum
+}
 
 // solvePartOne solves part two of the puzzle.
 func solvePartTwo(r io.Reader) (int, error) {
