52b47c1bf32cf049793e3963fbd75a63a8012a5e
teleivo <teleivo@users.noreply.github.com>
2023-12-16 12:10:16 +0100

    16/part 1: try but fail :joy:

--- 2023/16/main.go
+++ 2023/16/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"errors"
 	"fmt"
 	"io"
 	"os"
@@ -41,11 +42,159 @@ func run(w io.Writer, args []string) error {
 	return nil
 }
 
+type direction int
+
+const (
+	left direction = iota
+	right
+	up
+	down
+)
+
+type point struct {
+	row int
+	col int
+}
+
+var move map[direction]func(*beam) = map[direction]func(*beam){
+	left:  moveLeft,
+	right: moveRight,
+	up:    moveUp,
+	down:  moveDown,
+}
+
+func moveLeft(in *beam) {
+	in.dir = left
+	in.pos = point{row: in.pos.row, col: in.pos.col - 1}
+}
+func moveRight(in *beam) {
+	in.dir = right
+	in.pos = point{row: in.pos.row, col: in.pos.col + 1}
+}
+func moveUp(in *beam) {
+	in.dir = up
+	in.pos = point{row: in.pos.row - 1, col: in.pos.col}
+}
+func moveDown(in *beam) {
+	in.dir = down
+	in.pos = point{row: in.pos.row + 1, col: in.pos.col}
+}
+
+type beam struct {
+	pos point
+	dir direction
+}
+
 // solvePartOne solves part one of the puzzle.
 func solvePartOne(in []byte) (int, error) {
 	pattern := bytes.Fields(in)
-	_ = pattern
-	return 0, nil
+
+	energized := make(map[point]struct{})
+	beams := []*beam{{dir: right}}
+
+	for len(beams) > 0 {
+		currentBeam := beams[0]
+		fmt.Printf("currentBeam %v\n", currentBeam)
+
+		switch pattern[currentBeam.pos.row][currentBeam.pos.col] {
+		case '/':
+			energized[currentBeam.pos] = struct{}{}
+			if currentBeam.dir == right {
+				moveUp(currentBeam)
+				fmt.Printf("hit / moving up to %v\n", currentBeam)
+			} else if currentBeam.dir == left {
+				moveDown(currentBeam)
+				fmt.Printf("hit / from left moving down to %v\n", currentBeam)
+			} else { // currentBeam.dir == up || currentBeam.dir == down
+				// continue in the same beam.direction
+				mf, ok := move[currentBeam.dir]
+				if !ok {
+					return 0, errors.New("move func not found")
+				}
+				mf(currentBeam)
+				fmt.Printf("hit / from up or down so continue in the same direction%v\n", currentBeam)
+			}
+		case '\\':
+			energized[currentBeam.pos] = struct{}{}
+			if currentBeam.dir == right {
+				moveDown(currentBeam)
+				fmt.Printf("hit \\ moving down to %v\n", currentBeam)
+			} else if currentBeam.dir == left {
+				moveUp(currentBeam)
+				fmt.Printf("hit \\ from the left moving up to %v\n", currentBeam)
+			} else { // if currentBeam.dir == up || currentBeam.dir == down
+				// continue in the same direction
+				mf, ok := move[currentBeam.dir]
+				if !ok {
+					return 0, errors.New("move func not found")
+				}
+				mf(currentBeam)
+				fmt.Printf("hit \\ from left or right so continue in the same direction%v\n", currentBeam)
+			}
+		case '|':
+			energized[currentBeam.pos] = struct{}{}
+			if currentBeam.dir == up || currentBeam.dir == down {
+				// continue in the same direction
+				mf, ok := move[currentBeam.dir]
+				if !ok {
+					return 0, errors.New("move func not found")
+				}
+				mf(currentBeam)
+				fmt.Printf("hit | from up or down so continue in the same direction%v\n", currentBeam)
+			} else { // split the beam
+				downBeam := &beam{pos: currentBeam.pos}
+				moveDown(downBeam)
+				if isInBounds(downBeam, pattern) {
+					beams = append(beams, downBeam)
+				}
+				moveUp(currentBeam)
+				fmt.Printf("hit | splitting up %v and down %v\n", currentBeam, downBeam)
+			}
+		case '-':
+			energized[currentBeam.pos] = struct{}{}
+			if currentBeam.dir == left || currentBeam.dir == right {
+				// continue in the same direction
+				mf, ok := move[currentBeam.dir]
+				if !ok {
+					return 0, errors.New("move func not found")
+				}
+				mf(currentBeam)
+				fmt.Printf("hit - from left or right so continue in the same direction%v\n", currentBeam)
+			} else { // split the beam
+				rightBeam := &beam{pos: currentBeam.pos}
+				moveRight(rightBeam)
+				if isInBounds(rightBeam, pattern) {
+					beams = append(beams, rightBeam)
+				}
+				moveLeft(currentBeam)
+				fmt.Printf("hit - splitting left %v and right %v\n", currentBeam, rightBeam)
+			}
+		case '.':
+			mf, ok := move[currentBeam.dir]
+			if !ok {
+				return 0, errors.New("move func not found")
+			}
+			mf(currentBeam)
+			fmt.Printf("empty tile continue moving %v\n", currentBeam)
+		}
+
+		if !isInBounds(currentBeam, pattern) {
+			fmt.Printf("current beam out of bounds %v\n", currentBeam)
+			if len(beams) > 1 {
+				beams = beams[1:]
+			} else {
+				beams = nil
+			}
+		}
+	}
+
+	return len(energized), nil
+}
+
+func isInBounds(in *beam, field [][]byte) bool {
+	rows := len(field)
+	cols := len(field[0])
+	return in.pos.row >= 0 && in.pos.row < rows && in.pos.col >= 0 && in.pos.col < cols
 }
 
 // solvePartTwo solves part two of the puzzle.
